6e57d4a4503642132a859b684c91a56f5e5959a1
edu-jason-kim <edu.jason.kim@gmail.com>
2025-08-26 14:26:28 +0900

    typescript basic

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
+import type { NextFunction, Request, RequestHandler, Response } from "express";
 import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req: any, res: any) => {
+const handler = (req: Request, res: Response, next: NextFunction) => {
   res.send("Hello World!");
-});
+};
+
+const handler2: RequestHandler = (req, res, next) => {
+  res.send("Hello World!");
+};
+
+app.get("/", handler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
